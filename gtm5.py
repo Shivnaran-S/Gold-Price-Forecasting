b4c1e22493a6d0510d8a00e8e1c224af846a18bd
Shivnaran.S <139701365+Shivnaran-S@users.noreply.github.com>
2025-01-26 17:24:14 +0530

    Update gtm5.py

--- gtm5.py
+++ gtm5.py
@@ -411,10 +411,16 @@ def main():
     plt.show()
 
     #21-22
-    min_val = min(data['Morning']['1-Aug-21':'31-Jul-22'].min(), data['Evening']['1-Aug-21':'31-Jul-22'].min())
-    max_val = max(data['Morning']['1-Aug-21':'31-Jul-22'].max(), data['Evening']['1-Aug-21':'31-Jul-22'].max())
+    start_date = pd.to_datetime('1-Aug-21', format='%d-%b-%y')
+    end_date = pd.to_datetime('31-Jul-22', format='%d-%b-%y')
+    
+    data_filtered = data[(data['Date'] >= start_date) & (data['Date'] <= end_date)]
+    min_val = min(data_filtered['Morning'].min(), data_filtered['Evening'].min())
+    max_val = max(data_filtered['Morning'].min(), data_filtered['Evening'].min())
+    #min_val = min(data['Morning']['1-Aug-21':'31-Jul-22'].min(), data['Evening']['1-Aug-21':'31-Jul-22'].min())
+    #max_val = max(data['Morning']['1-Aug-21':'31-Jul-22'].max(), data['Evening']['1-Aug-21':'31-Jul-22'].max())
 
-    data.plot(xlim=['1-Aug-21','31-Jul-22'],ylim=[min_val,max_val],figsize=(15,4))
+    data.plot(xlim=[start_date,end_date],ylim=[min_val,max_val],figsize=(15,4))
     plt.xlabel("Date")
     plt.ylabel("Gold Price")
     plt.title("Gold Price for the year 2021-22")
